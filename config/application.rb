58bb9b93a662db515c9ca1dc479c210e12020c6f
Pablo Peuscovich <ppeusco@gmail.com>
2023-12-12 13:29:01 -0300

    Update Readme.rdoc

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module Rails4App
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.precompile += ['posts.scss']
   end
 end
